c3066ddc4712c0363ca57304496fb787ce79228f
gabriel-fercav <gabriel.f.cavalcante@hotmail.com>
2025-10-27 17:26:11 -0300

    Added page buttons

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { Button } from '@/components/ui/button'
 import ArrowLeft from '@/assets/arrow-left.svg'
 import ArrowRight from '@/assets/arrow-right.svg'
 
 const Footer = ({ goToPage, isFetching, page, searchValue }) => {
+  const [pageButtons] = useState([1, 2, 3])
+
   if (searchValue) return null
 
   return (
-    <div className="flex-center pb-3 bg-background">
+    <div className="flex-center pb-3 bg-background mt-15 mb-10 gap-3">
       <Button
         className="text-md"
         variant="ghost"
@@ -18,6 +20,17 @@ const Footer = ({ goToPage, isFetching, page, searchValue }) => {
         <img src={ArrowLeft} className="w-3 h-3" alt="Seta para esquerda" />
         Anterior
       </Button>
+      {pageButtons?.map((newPage) => {
+        return (
+          <Button
+            onClick={() => goToPage(newPage)}
+            variant={page === newPage ? 'default' : 'outline'}
+            className="text-md cursor-default w-8 rounded-lg"
+          >
+            {newPage}
+          </Button>
+        )
+      })}
       <Button
         className="text-md"
         variant="ghost"
